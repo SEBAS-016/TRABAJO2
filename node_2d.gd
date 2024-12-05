7f354c0117e0a4d2a8a1e595404e4111c8775690
SEBAS-016 <sebasjr016@gmail.com>
2024-12-04 18:25:05 -0600

    avancez
    las preguntas finalisadas de prueba

--- node_2d.gd
+++ node_2d.gd
@@ -8,4 +8,26 @@ func _ready():
 	randomize()
 	preguntar()
 	
-func pregunta()
+func pregunta():
+	pregunta_numero = randi() % 3
+	print( respuesta)
+
+	if pregunta_numero == 1:
+		print ("de que color son los mares ")
+		$p.text = "de que color son "
+		$r1.text = "azul"
+		$r2.text = "verde"
+		$r3.text = "el oceno no tiene color "
+		$r4.text = "no se "
+	 if pregunta_numero == 2 :
+		print ("de q color es la luna ")
+		$p.text = "de q color es la luna"
+		$r1.tex = "azul"
+		$r2.text = "blanco"
+		$r3.text = "amarilla"
+		$r4.text = "no se "
+	
+
+			
+func ressponder():
+ 	 if pregunta_numero == 1 :
