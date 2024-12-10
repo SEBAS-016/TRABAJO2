d99fd3dd7d312811ceb7b9d1a28aa37e703e98c6
SEBAS-016 <sebasjr016@gmail.com>
2024-12-09 18:15:34 -0600

    avancez de clase
    se ajusto el tiempo y se vana comenzar a ser pregunta ya de anatomia

--- node_2d.gd
+++ node_2d.gd
@@ -1,48 +1,67 @@
 extends Node2D
 
 # variable para definir que pregunta toca
+var tiempo_respuesta 
 var pregunta_numero
-var respuesta 
-# Called when the node enters the scene tree for the first time.
+var respuesta
+var preguntas_correctas = []
+const total = 4
+var time = 10 
+var t
+var delta
+
 func _ready():
 	randomize()
 	preguntar()
+	print()
+	
+func _process(delta):
+	$RESTANTE.text = str(round($tiempio_respuesta.time_left))
 	
 func preguntar():
-	pregunta_numero = randi() % 4
-	print( respuesta)
+	var cantidad_elementos = len(preguntas_correctas)
+	if cantidad_elementos == total:
+		get_tree().change_scene_to_file("res://felicitaciones.tscn")
+	else:
+		pregunta_numero = randi() % 4
+		print( respuesta) 
+		if pregunta_numero in preguntas_correctas:
+			preguntar()
+			print(pregunta_numero, "está en la lista de números.")
+		else:
+			print(pregunta_numero, "no está en la lista de números.")
 
-	if pregunta_numero == 1:
-		print ("de que color son los mares ")
-		$p.text = "de que color son "
-		$r1.text = "azul"
-		$r2.text = "verde"
-		$r3.text = "el oceno no tiene color "
-		$r4.text = " no se "
-	
-	if pregunta_numero == 2 :
-		print ("de q color es la luna ")
-		$p.text = "de q color es la luna"
-		$r1.text = "azul"
-		$r2.text = "blanco"
-		$r3.text = "amarilla"
-		$r4.text = "no se"
-		
-	if pregunta_numero == 3 :
-		print ("de q color es la sol ")
-		$p.text = "de q color es la sol"
-		$r1.text = "azul"
-		$r2.text = "blanco"
-		$r3.text = "amarilla"
-		$r4.text = "no se"
-	
-	if pregunta_numero == 4:
-		print ("de que color son las nubez  ")
-		$p.text = "de que color son las nubez  "
-		$r1.text = "azul"
-		$r2.text = "verde"
-		$r3.text = "blencas"
-		$r4.text = " no se "
+			if pregunta_numero == 1:
+				print ("de que color son los mares ")
+				$p.text = "de que color son "
+				$r1.text = "azul"
+				$r2.text = "verde"
+				$r3.text = "el oceno no tiene color "
+				$r4.text = " no se "
+			
+			if pregunta_numero == 2 :
+				print ("de q color es la luna ")
+				$p.text = "de q color es la luna"
+				$r1.text = "azul"
+				$r2.text = "blanco"
+				$r3.text = "amarilla"
+				$r4.text = "no se"
+				
+			if pregunta_numero == 3 :
+				print ("de q color es la sol ")
+				$p.text = "de q color es la sol"
+				$r1.text = "azul"
+				$r2.text = "blanco"
+				$r3.text = "amarilla"
+				$r4.text = "no se"
+			
+			if pregunta_numero == 4:
+				print ("de que color son las nubez  ")
+				$p.text = "de que color son las nubez  "
+				$r1.text = "azul"
+				$r2.text = "verde"
+				$r3.text = "blencas"
+				$r4.text = " no se "
 		
 func responder():
 	if pregunta_numero == 0:
@@ -55,6 +74,7 @@ func responder():
 			
 		if respuesta == 3:
 			$respuesta.text = "CORRECTA"
+			preguntas_correctas.append(0)
 			
 		if respuesta == 4:
 			$respuesta.text = "INCORRECTA"
@@ -64,10 +84,12 @@ func responder():
 			$respuesta.text = "INCORRECTA"
 			
 		if respuesta == 2:
-			$rrespuesta.text = "INCORRECTA"
+			$respuesta.text = "INCORRECTA"
 			
 		if respuesta == 3:
 			$respuesta.text = "CORRECTA"
+			preguntas_correctas.append(1)
+			
 			
 		if respuesta == 4:
 			$respuesta.text = "INCORRECTA"
@@ -87,6 +109,7 @@ func responder():
 			
 		if respuesta == 4:
 			$respuesta.text = "CORRECTA"
+			preguntas_correctas.append(2)
 		
 
 	if pregunta_numero == 3:
@@ -100,6 +123,7 @@ func responder():
 			
 		if respuesta == 3:
 			$respuesta.text = "CORRECTA"
+			preguntas_correctas.append(3)
 			
 		if respuesta == 4:
 			$respuesta.text = "INCORRECTA"
@@ -109,13 +133,14 @@ func _on_r_1_pressed():
 	respuesta = 1
 	responder()
 	preguntar()
+	print(preguntas_correctas)
 
 
 func _on_r_2_pressed():
 	respuesta = 2
 	responder()
 	preguntar()
-
+	print(preguntas_correctas)
 
 
 
@@ -123,6 +148,7 @@ func _on_r_3_pressed():
 	respuesta = 3
 	responder()
 	preguntar()
+	print(preguntas_correctas)
 
 
 
@@ -130,3 +156,8 @@ func _on_r_4_pressed():
 	respuesta = 4
 	responder()
 	preguntar()
+	print(preguntas_correctas)
+
+func _on_tiempio_respuesta_timeout():
+	get_tree().change_scene_to_file("res://gameover.tscn")
+	
